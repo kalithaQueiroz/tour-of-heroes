4f24073120a65f534cdc605446176bad8feefb85
kalithaQueiroz <69155037+kalithaQueiroz@users.noreply.github.com>
2022-12-22 08:54:03 -0300

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
+  declarations: [AppComponent, HeroesComponent],
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, HeroesComponent],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
